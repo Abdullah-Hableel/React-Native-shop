c1213d1cab5555944ca100da735da925d34730dd
abdullah <aboodk841@gmail.com>
2025-08-04 16:18:10 +0300

    done

--- app/index.tsx
+++ app/index.tsx
@@ -69,36 +69,36 @@
 //     </View>
 //   );
 // }
-import ProductItem from "@/Components/ProductItem";
-import { Image, Text, View } from "react-native";
-export default function Index() {
-  return (
-    <View style={{ flex: 1 }}>
-      {/* First section */}
-      <View
-        style={{
-          alignItems: "center",
-          margin: 10,
-        }}
-      >
-        <Text style={{ margin: 10, fontSize: 30, fontWeight: "bold" }}>
-          abdullah store
-        </Text>
-        <Text style={{ margin: 5, fontSize: 20 }}>cocees and breads</Text>
-        <Image
-          source={{
-            uri: "https://media-cdn.tripadvisor.com/media/photo-s/08/a6/b5/17/great-american-cookies.jpg",
-          }}
-          style={{ margin: 10, width: 200, height: 200 }}
-        />
-      </View>
-      {/* Item 1 */}
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
+// import ProductItem from "@/Components/ProductItem";
+// import { Image, Text, View } from "react-native";
+// export default function Index() {
+//   return (
+//     <View style={{ flex: 1 }}>
+//       {/* First section */}
+//       <View
+//         style={{
+//           alignItems: "center",
+//           margin: 10,
+//         }}
+//       >
+//         <Text style={{ margin: 10, fontSize: 30, fontWeight: "bold" }}>
+//           abdullah store
+//         </Text>
+//         <Text style={{ margin: 5, fontSize: 20 }}>cocees and breads</Text>
+//         <Image
+//           source={{
+//             uri: "https://media-cdn.tripadvisor.com/media/photo-s/08/a6/b5/17/great-american-cookies.jpg",
+//           }}
+//           style={{ margin: 10, width: 200, height: 200 }}
+//         />
+//       </View>
+//       {/* Item 1 */}
+//       <View
+//         style={{
+//           flexDirection: "row",
+//           alignItems: "center",
+//           justifyContent: "center",
+//         }}
       >
         {/* <View
           style={{
@@ -119,14 +119,64 @@ export default function Index() {
         </View> */}
 
         {/* Item 2 */}
-        <View
+        {/* <View
           style={{
             alignItems: "center",
-            margin: 10,
+            margin: 20,
           }}
         ></View>
       </View>
-      <ProductItem />
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        <ProductItem
+          name=" asd rw"
+          cokeisImage="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTA4L3Jhd3BpeGVsX29mZmljZV8yOV90b3Bfdmlld19waG90b19vZl9kYXJrX2Nob2NvbGF0ZV9zb2Z0X2Nvb2tpZV8wNDc3OTEwNy1hMWZjLTQ5N2MtOTRhMC0wM2UzMDVmYTUxOTcucG5n.png"
+        />
+        <ProductItem
+          name=" kinder"
+          cokeisImage="https://t4.ftcdn.net/jpg/06/76/66/39/360_F_676663959_jcE1oIYNF9qgx7FTSSMy6DHxsRkFHTyO.jpg"
+        />
+      </View>
     </View>
+  );
+} */}
+// index.tsx
+
+import { Image, Text, View, ScrollView } from "react-native";
+import ProductItem from "../components/ProductItem";
+import { Products } from "../data/Products"; 
+
+export default function Index() {
+  return (
+    <ScrollView style={{ flex: 1 }}>
+      
+      <View style={{ alignItems: "center", margin: 10 }}>
+        <Text style={{ margin: 10, fontSize: 30, fontWeight: "bold" }}>
+          abdullah store
+        </Text>
+        <Text style={{ margin: 5, fontSize: 20 }}>cocees and breads</Text>
+        <Image
+          source={{
+            uri: "https://media-cdn.tripadvisor.com/media/photo-s/08/a6/b5/17/great-american-cookies.jpg",
+          }}
+          style={{ margin: 10, width: 200, height: 200 }}
+        />
+      </View>
+
+      {/* عرض المنتجات ديناميكياً */}
+      <View style={{ flexDirection: "row", justifyContent: "center", flexWrap: "wrap" }}>
+        {Products.map((item) => (
+          <ProductItem
+            key={item.id}
+            title={item.title}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
+      </View>
+    </ScrollView>
   );
 }
---
+++ data/Products.tsx
@@ -0,0 +1,17 @@
+export const Products = [
+  {
+    id: 1,
+    title: "Normal Cookies",
+    price: 2,
+    image:
+      "https://t4.ftcdn.net/jpg/06/76/66/39/360_F_676663959_jcE1oIYNF9qgx7FTSSMy6DHxsRkFHTyO.jpg",
+  },
+  {
+    id: 2,
+    title: "Dark Cookies",
+    price: 4,
+    image:
+      "https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTA4L3Jhd3BpeGVsX29mZmljZV8yOV90b3Bfdmlld19waG90b19vZl9kYXJrX2Nob2NvbGF0ZV9zb2Z0X2Nvb2tpZV8wNDc3OTEwNy1hMWZjLTQ5N2MtOTRhMC0wM2UzMDVmYTUxOTcucG5n.png",
+  },
+];
+export default Products;
